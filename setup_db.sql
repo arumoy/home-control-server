80e7d5335c95d321f2d15e4eddb77ec4a06b779f
Arumoy Chakraboty <arumoy0@gmail.com>
2015-02-25 21:00:17 +0530

    DATABASE INITIALIZATION FILE

---
+++ setup_db.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE home_cont;
+USE home_cont;
+CREATE TABLE `dat` (
+      `user_id` varchar(15) DEFAULT NULL,
+      `room_alias` varchar(10) DEFAULT NULL,
+      `appliance_alias` varchar(8) DEFAULT NULL,
+      `appl_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+      `status` varchar(3) NOT NULL,
+      PRIMARY KEY (`appl_id`),
+      UNIQUE KEY `appl_id` (`appl_id`)
+);
+
+
+CREATE TABLE `userpass` (
+      `username` varchar(15) NOT NULL,
+      `pass` varchar(32) DEFAULT NULL,
+      PRIMARY KEY (`username`)
+);
