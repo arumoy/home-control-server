5331bb1caa5cc986f879827a9d8fdbcac3eef4d0
Arumoy Chakraborty <arumoy0@gmail.com>
2015-02-26 19:49:09 +0530

    Added DB Setup SQL

---
+++ fill.sql
@@ -0,0 +1,18 @@
+insert into dat values( 'arumoy', 'in_room', 'cfl', 0, 'on');
+insert into dat values( 'arumoy', 'in_room', 'fan', 0, 'on');
+insert into dat values( 'arumoy', 'in_room', 'pc', 0, 'on');
+insert into dat values( 'arumoy', 'in_room', 'router', 0, 'on');
+insert into dat values( 'arumoy', 'out_room', 'tv', 0, 'off');
+insert into dat values( 'arumoy', 'out_room', 'lamp', 0, 'on');
+insert into dat values( 'arumoy', 'out_room', 'fan', 0, 'off');
+insert into dat values( 'arumoy', 'kitchen', 'cfl', 0, 'on');
+insert into dat values( 'arumoy', 'kitchen', 'induction', 0, 'on');
+insert into dat values( 'arumoy', 'kitchen', 'mixer', 0, 'off');
+insert into dat values( 'arumoy', 'kitchen', 'exhaust', 0, 'off');
+insert into dat values( 'tom', 'dining', 'cfl', 0, 'on');
+insert into dat values( 'tom', 'dining', 'fan', 0, 'on');
+insert into dat values( 'tom', 'dining', 'fridge', 0, 'on');
+insert into dat values( 'tom', 'dining', 'RO', 0, 'off');
+insert into dat values( 'tom', 'toilet', 'cfl', 0, 'off');
+insert into dat values( 'tom', 'toilet', 'exhaust', 0, 'off');
+
---
+++ fill_up.sql
@@ -0,0 +1,2 @@
+insert into userpass values('arumoy', 'e2075474294983e013ee4dd2201c7a73');
+insert into userpass values('tom', '30035607ee5bb378c71ab434a6d05410');
