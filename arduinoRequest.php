53242669a2d892bb1f48946cab42eb6eeff52af4
Arumoy Chakraboty <arumoy0@gmail.com>
2015-03-11 15:19:52 +0530

    ardunoRequest handler finalized... Well, probably ;-P

--- arduinoRequest.php
+++ arduinoRequest.php
@@ -1,20 +1,12 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: arumoy
- * Date: 11/3/15
- * Time: 12:33 PM
- */
 $client_ip = $_SERVER['REMOTE_ADDR'];
 $link = mysqli_connect('localhost', 'arumoy', 'ohms', 'home_cont', '3306');
-$usn = filter_input(INPUT_GET, usr_id);
+$usn = filter_input(INPUT_GET, 'id');
 $query = "UPDATE home_cont.userpass SET home_ip = '{$client_ip}' WHERE username = '{$usn}'";
 mysqli_query($link, $query);
-$query = "select appl_id, status from dat where user_id = '{$usn}'";
-$result = mysqli_query($link, $query);
-$bell = "";
-while($row = mysql_fetch_array($result)) {
-    $bell = $bell." "."{$row['status']}";
+$query = "select status from home_cont.dat where user_id = '{$usn}'";
+$res = mysqli_query($link, $query);
+while($row = mysqli_fetch_array($res)) {
+    echo $row['status']."|";
 }
-echo $bell;
 ?>
